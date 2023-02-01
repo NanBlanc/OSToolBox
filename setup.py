7ade15e3c79204498369a371150e20b1c1648b1b
ostocker <olivier.stocker.1@ulaval.ca>
2023-02-01 11:39:03 -0500

    dependencies intergartion, rdy for 1.2

--- setup.py
+++ setup.py
@@ -13,11 +13,12 @@ Project:
 from setuptools import setup
 
 setup(name='OSToolBox',
-      version='1.1',
-      description='implement many basic function as tools for GDAL, numpy, etc...',
+      version='1.2',
+      description='implement many basic function as tools for GIS coding',
       url='https://github.com/NanBlanc/OSToolBox',
       author='Olivier Stocker',
       author_email='stocker.olivier@gmail.com',
       license='MIT',
-      packages=['OSToolBox'],
+      install_requires=['numpy','matplotlib'], #FOR TEST.PYPI: DEPENDENCIES CHECK CAN NOT CARRIED AS TEST.PYPI ISNT MAINTAINED FOR DEPENDENCIES
+      packages=['OSToolBox','GDALToolBox'],
       zip_safe=False)
